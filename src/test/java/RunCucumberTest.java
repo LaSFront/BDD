fd1a589c92f8d74ffae14d32bfee4b04f6e93fda
“Larisa” <lsergeevna1978@mail.ru>
2023-08-04 12:19:37 +0400

    Initial commit

--- src/test/java/RunCucumberTest.java
+++ src/test/java/RunCucumberTest.java
@@ -4,6 +4,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {"pretty", "summary"},
         features = {"classpath:features/Auth.feature"},
         glue = {"ru.netology.web"})
 public class RunCucumberTest {
